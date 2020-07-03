d0e38182bf98930ac876040fd1ba975eb2a6f91e
Carlos Alvarez Moreno <ceam1710@gmail.com>
2020-07-03 11:17:31 -0500

    Final Vue

---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias fugiat voluptas harum repudiandae fugit accusamus amet temporibus ipsa voluptates molestias sint atque nobis quisquam odit, illum officiis aliquid dolores et.</p>
+  </div>
+</template>
